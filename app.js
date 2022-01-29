ef290d239df2600587ac4e6de1615daab37f35ee
EugeneR <eugenerillo@gmail.com>
2022-01-29 00:49:56 -0500

    getGuess works prompt meh

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum 
+  },
+  getGuess: function(){
+    let guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
+    while (parseInt(guess) !== NaN && parseInt(guess) >= this.smallestNum && parseInt(guess) <= this.biggestNum)
+    return guess
   }
 }
+console.log(game.getGuess())
